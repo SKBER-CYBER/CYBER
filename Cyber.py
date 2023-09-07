404f2cc7ee0e8d44b2d5047d4f80a5877ff92641
Jáhìd hassan <140346514+SKBER-CYBER@users.noreply.github.com>
2023-09-07 12:07:47 +0600

    Update Cyber.py

--- Cyber.py
+++ Cyber.py
@@ -6,7 +6,7 @@ bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
     os.system('xdg-open https://facebook.com/groups/790381136153614/')
-    import Sad
+    import jahid
 else:
     exit('\033[1;31m[×] Sorry Device Not Support')
  
